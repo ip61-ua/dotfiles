7a75f39a02f351246d68c6c2074b8941c100b282
ip61-ua <ip61@alu.ua.es>
2026-02-07 18:56:33 +0100

    modify .gitignore and include init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,78 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(leuven-dark))
+ '(face-font-family-alternatives
+   '(("JetBrains Mono NL" "courier" "fixed")
+     ("Monospace Serif" "Courier 10 Pitch" "Consolas" "Courier Std"
+      "FreeMono" "Nimbus Mono L" "courier" "fixed")
+     ("courier" "CMU Typewriter Text" "fixed")
+     ("Sans Serif" "helv" "helvetica" "arial" "fixed")
+     ("helv" "helvetica" "arial" "fixed")))
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(column-number-mode)
+(global-display-line-numbers-mode)
+(desktop-save-mode 1)
+(windmove-default-keybindings)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(use-package corfu
+  :ensure t
+  :custom
+  (corfu-auto t)
+  (corfu-quit-no-match 'separator)
+  (corfu-cycle t)
+  :init
+  (global-corfu-mode))
+
+(use-package eglot
+  :hook ((c-mode c++-mode) . eglot-ensure)
+  :bind ("C-c r" . eglot-rename)
+  :config
+  (add-to-list 'eglot-server-programs
+	       '((c++-mode c-mode)
+		 . ("clangd"
+		    "--background-index"
+		    "--clang-tidy"
+		    "--header-insertion=iwyu"
+		    "--completion-style=detailed"
+		    "--function-arg-placeholders")))
+  (add-hook 'before-save-hook
+	    (lambda ()
+	      (when (derived-mode-p 'c++-mode 'c-mode)
+		(eglot-format-buffer)))))
+
+(setq-default c-basic-offset 4)
+
+(use-package eldoc
+  :init
+  (global-eldoc-mode))
+
+(use-package yasnippet
+  :config
+  (yas-global-mode 1))
+
+(use-package yasnippet-snippets)
+
+(setq compilation-scroll-output t)
+(setq gdb-many-windows t)
+(global-set-key (kbd "<f5>") 'recompile)
