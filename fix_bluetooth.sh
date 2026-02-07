f723ec307fa698e775da06b97c95d20c9ec9f4ed
ip61-ua <ip61@alu.ua.es>
2026-02-07 19:08:09 +0100

    include fix_bluetooth.sh

---
+++ fix_bluetooth.sh
@@ -0,0 +1,47 @@
+TIEMPO_ESPERA=1
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "El programa requiere permisos de elevación."
+  pkexec sudo -p bash $(realpath $0)
+  exit 0
+fi
+
+while true; do
+
+  echo " >>> Intento #$TIEMPO_ESPERA. <<< "
+
+  if bluetoothctl list | grep -q "Controller"; then
+    echo "Controlador detectado."
+    break
+  fi
+
+  systemctl stop bluetooth
+
+  rmmod -v bnep
+  rmmod -v btusb
+  rmmod -v bluetooth
+  rmmod -v btintel
+  rmmod -v btrtl
+  rmmod -v btmtk
+  rmmod -v btbcm
+  rmmod -v rfcomm
+  rmmod -v bluetooth
+
+  modprobe -v bnep
+  modprobe -v btusb
+  modprobe -v bluetooth
+  modprobe -v btintel
+  modprobe -v btrtl
+  modprobe -v btmtk
+  modprobe -v btbcm
+  modprobe -v rfcomm
+  modprobe -v bluetooth
+
+  systemctl start bluetooth
+
+  sleep $TIEMPO_ESPERA
+  TIEMPO_ESPERA=$((TIEMPO_ESPERA+1))
+done
+
+echo "Hecho."
+exit 0
